51c0bba771c806cdb6bc41df0d54af3366f8713f
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-07-05 14:49:24 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,11 @@
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each {|season,contestant| #puts "#{season}, #{contestant}" 
+    contestant.each {|info_type, info| #puts "#{info_type}, #{info}"   
+      
+    }
+  }
+  
 end
 
 def get_contestant_name(data, occupation)
